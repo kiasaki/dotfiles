f156b9db64b60311314a72efecfd2b6e0110427a
Frederic Tulsidin <frederic@tulsidin.ca>
2020-12-06 19:37:48 -0500

    update ubuntu setup

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -5,36 +5,36 @@ pushd ~ >/dev/null
 
 sudo apt-get install -qq -y software-properties-common \
   build-essential \
-  libfreetype6-dev \
   make \
   cmake \
   curl \
-  fontconfig \
   git \
   git-lfs \
   htop \
   mosh \
   tmux \
   jq \
-  silversearcher-ag \
   neovim \
-  redis-server \
+  silversearcher-ag \
   postgresql \
-  python3-pip \
-  xorg-dev \
-  xclip \
-  feh
+  redis-server \
+  python3-pip
+# libfreetype6-dev \
+#  fontconfig \
+#  xclip \
+#  xorg-dev \
+#  feh
 
-sudo systemctl start redis-server
-sudo systemctl enable redis-server
-git lfs install
-which ansible>/dev/null || pip3 install --user ansible virtualenv awscli
+sudo systemctl start redis-server && sudo systemctl enable redis-server || sudo service redis-server start
+sudo systemctl start postgresql && sudo systemctl enable postgresql || sudo service postgresql start
 sudo -u postgres psql -c "create user $USER with superuser;" || true
 sudo -u postgres psql -c "create database $USER with owner $USER;" || true
+git lfs install
+which ansible>/dev/null || pip3 install --user ansible virtualenv awscli
 
 sudo cp -r ~/dotfiles/support/fonts/go /usr/share/fonts/go
 sudo cp -r ~/dotfiles/support/fonts/input /usr/share/fonts/input
-fc-cache -f -v
+fc-cache -f -v || true
 
 mkdir -p ~/bin ~/code/{dev,repos,venv,go}
 mkdir -p ~/.vim/{autoload,colors,syntax} ~/.config/nvim/{autoload,colors,syntax}
